3691912186c19d021cc38f933b26ae7d9b43399d
jtaylor <james.jm.taylor@gmail.com>
2018-12-13 09:21:17 -0800

    Added touch events, currently the background, not the Coin, is receiving the touch events

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+    //sppeed in degrees of rotation per frame
+    public float xSpeed = 5f;
+    public float ySpeed = 0f;
+    public float zSpeed = 0f;
+
+    void OnTouchDown()
+    {
+        xSpeed = 0f;
+    }
+    void OnTouchUp()
+    {
+        xSpeed = 5f;
+    }
+    void OnTouchStay()
+    {
+        xSpeed = 0f;
+    }
+    void OnTouchExit()
+    {
+        xSpeed = 5f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(xSpeed,ySpeed,zSpeed);
+    }
+    
+}
